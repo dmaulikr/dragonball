818c6c7a643502c51164a4e2e6395761e6c6c430
Kevin Wagner <kevinwagner1994@gmx.de>
2013-01-08 17:22:37 +0100

    Initial Commit

---
+++ Taxifahrer/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Taxifahrer
+//
+//  Created by Kevin Wagner on 08.01.13.
+//  Copyright (c) 2013 Kevin Wagner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Taxifahrer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Taxifahrer
+//
+//  Created by Kevin Wagner on 08.01.13.
+//  Copyright (c) 2013 Kevin Wagner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
