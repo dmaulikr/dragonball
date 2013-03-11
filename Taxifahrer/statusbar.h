a424eb5153e7f48ab3a391c31549cbbf6e9ca1a1
Kevin Wagner <kevinwagner1994@gmx.de>
2013-03-11 12:07:58 +0100

    Das Design der Statusleiste wurde erschaffen. Da ich keine Vorlage hatte, habe ich Grafiken nach Geschmack genommen oder selbst entworfen

---
+++ Taxifahrer/statusbar.h
@@ -0,0 +1,13 @@
+//
+//  statusbar.h
+//  Taxifahrer
+//
+//  Created by Kevin Wagner on 11.03.13.
+//  Copyright (c) 2013 Kevin Wagner. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface statusbar : ViewController
+
+@end
---
+++ Taxifahrer/statusbar.m
@@ -0,0 +1,38 @@
+//
+//  statusbar.m
+//  Taxifahrer
+//
+//  Created by Kevin Wagner on 11.03.13.
+//  Copyright (c) 2013 Kevin Wagner. All rights reserved.
+//
+
+#import "statusbar.h"
+
+@interface statusbar ()
+
+@end
+
+@implementation statusbar
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
